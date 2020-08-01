d5d81bec2304076bfe9342167b608e9aa243907e
Samuel Tobia <sam@system76-pc.localdomain>
2020-07-31 21:39:07 -0400

    more on turn based

---
+++ src/TurnBasedGame/index.ts
@@ -0,0 +1,84 @@
+/*
+
+Turnbased
+  - each turn one or more people need to do an action
+    - The people listening
+  - Each player encrypts their move and sends to everyone else
+    - each person who receives the move, responds with an accept
+  - When all people have sent their move
+    - Each person sends their decryption key
+
+
+*/
+
+type MoveListener = (move: string)=>void
+
+interface Player {
+  id: string;
+  sendMove(move: string): Promise<boolean>;
+  onRecieveMove(listener: MoveListener)
+}
+
+type PlayerTurn = {
+  id: string
+  encryptedMove: string,
+  decryptionKey: string
+}
+
+class Turn {
+  selfId: string;
+  allPlayers: {
+    [id: string]: Player
+  }
+  expectingPlayers: Array<string>;
+  expectingPlayersValues: {
+    [string: id]: {
+      move: string,
+      decryptionKey: string
+    }
+  } = {}
+
+  constructor(
+    allPlayers: Array<Player>,
+    expectingPlayers: Array<string>
+  ){
+    this.expectingPlayers = expectingPlayers;
+    this.allPlayers = allPlayers.reduce(()=>{
+
+    }, {})
+  }
+
+  sendMove(move: string){
+    return Promise.resolve().then(()=>{
+      if(!this.expectingPlayers.some((id)=>{
+        return id === this.selfId
+      })){
+        throw new Error("")
+      }
+
+      Promise.resolveObject.values(this.allPlayers).
+    })
+  }
+  broadcastMoveToPlayers(move: string, players: Array<Player>){
+    var playersWith Error: Array<Player> = []
+    return Promise.all(players.map((player)=>{
+      return player.sendMove(move).catch((error)=>{
+        console.error(error);
+        playersWithError.push(player)
+      })
+    })).then(()=>{
+      if(playersWithError.length > 0){
+        return this.broadcastMoveToPlayers(
+          move, playersWithError
+        )
+      }
+    })
+  }
+
+  sendEncryptionKey(){
+
+  }
+
+
+
+}
