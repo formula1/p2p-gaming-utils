574aa9c451f68d35b7400a864c69c9cb2c90c151
Samuel Tobia <sam@system76-pc.localdomain>
2022-01-19 19:50:04 -0800

    Generic Code Feature: added ability to set frames per second, run function and an always run even if the time isn't ready

--- src/util/Clock.ts
+++ src/util/Clock.ts
@@ -1,24 +1,40 @@
 
+type FrameFunction = (frame: number)=>any;
+type CallBackFunction = ()=>any;
 
 export class Clock {
   msPerFrame: number;
   lastTime: number
   currentDelta: number;
   currentFrame: number = 0;
-  constructor(framesPerSecond: number){
-    this.msPerFrame = Math.floor(1/framesPerSecond * 1000)
+
+  deltaFunction: FrameFunction;
+  alwaysRun: CallBackFunction | void;
+
+  constructor(
+    framesPerSecond: number,
+    deltaFunction: FrameFunction,
+    alwaysRun?: CallBackFunction
+  ){
+    this.msPerFrame = Math.floor(1* 1000/framesPerSecond)
     this.lastTime = Date.now()
+    this.deltaFunction = deltaFunction;
+    this.alwaysRun = alwaysRun
   }
   checkTime(): boolean{
     var lastTime = this.lastTime
     var curTime = Date.now();
     var delta = curTime - lastTime;
+    if(this.alwaysRun){
+      this.alwaysRun()
+    }
     if(delta < this.msPerFrame){
       return false
     }
     this.currentDelta = delta;
     this.lastTime = curTime - (delta - this.msPerFrame);
     this.currentFrame++;
+    this.deltaFunction(this.currentFrame);
     return true;
   }
 }
